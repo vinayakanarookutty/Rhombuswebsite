ebc9e2358c751b8ec352494c36784723795f4d83
Vinayak S <vinayaksukhalal@gmail.com>
2024-10-20 12:05:43 +0530

    updated

---
+++ Desktop/gokeral/corestone_website/src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+import { Routes, Route,  } from 'react-router-dom';
+import HomePage from './HomePage';
+
+
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+    </Routes>
+  );
+};
+
+export default App;
